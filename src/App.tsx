1e6b18001113f13b883caac36d76a3bd60f18444
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2023-10-08 20:30:33 +0200

    fix: app imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import './App.scss';
 
-import helloMascot from './assets/helloMascot.svg';
-import smilingMascot from './assets/smilingMascot.svg';
 // eslint-disable-next-line import/no-unresolved
 import DVDLogo from './dvd/DVDLogo';
 
@@ -9,8 +7,6 @@ function App() {
   return (
     <>
       <DVDLogo width={window.innerWidth} height={window.innerHeight} />
-      <img src={helloMascot} width='100' alt='' />
-      <img src={smilingMascot} width='100' alt='' />
       <h1>Magnus Byrkjeland</h1>
     </>
   );
