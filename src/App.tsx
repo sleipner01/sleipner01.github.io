44aea1582385327537d6e28825557130551f367e
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2023-12-09 18:46:12 +0000

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import './App.scss';
 
+import { useState } from 'react';
+
 import DVDLogo from './dvd/DVDLogo';
 
 function App() {
+  const [windowSize, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+  // useEffect(() => {
+  //   document.addEventListener(window.onresize, setWindowSize({ width: window.innerWidth, height: window.innerHeight }));
+
+  //   return () => {
+  //     document.removeEventListener(window.onresize);
+  //   };
+  // });
+
   return (
     <>
-      <DVDLogo width={window.innerWidth} height={window.innerHeight} />
+      <DVDLogo width={windowSize.width} height={windowSize.height} />
       <h1>Magnus Byrkjeland</h1>
     </>
   );
