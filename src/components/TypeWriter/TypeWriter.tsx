e46de7c0416e6a622c63e1269081cab5c879f44e
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2023-12-09 20:56:22 +0000

    progress

--- src/components/TypeWriter/TypeWriter.tsx
+++ src/components/TypeWriter/TypeWriter.tsx
@@ -7,18 +7,31 @@ const TypeWriter = () => {
       <p className='second'>Sick right!?</p>
       <p className='third'>Maybe not what you thought you would find...</p>
       <p className='fourth'>but I wanted the page to stand out!</p>
-      <p className='fifth'>You see, I am kind of influenced by my dad.</p>
-      <p className='sixth'>He is an airline pilot.</p>
+      <p className='fifth'>You see, in good Norwegian I&apos;m &quot;miljøskadd&quot;...</p>
+      <p className='sixth'>My dad is an airline pilot.</p>
       <p className='seventh'>So this page looks like the</p>
-      <p className='eighth'>communication displays found i the cockpit.</p>
+      <p className='eighth'>communication displays found in the cockpit.</p>
       <p className='ninth'>Though it kind of looks like an old computer screen too!</p>
       <br />
-      <p className='tenth'>Anyways...</p>
+      <p className='tenth'>Anyway...</p>
       <p className='eleventh'>You can find my Github, LinkedIn &apos;n stuff below:</p>
       <div className='twelvth socials-container'>
-        <div>Github</div>
-        <div>Linkedin</div>
+        <div className='github'>
+          <a href='#' target='_blank'>
+            Github
+          </a>
+        </div>
+        <div className='linkedin'>
+          <a href='#' target='_blank'>
+            Linkedin
+          </a>
+        </div>
       </div>
+      <p className='thirteenth'>Wow! You&apos;re still here?? Impressive!</p>
+      <p className='fourteenth'>K&apos; then, I have some more to show you...</p>
+      <p className='fifteenth'>
+        <a className='link'>Click me ❤️</a>
+      </p>
     </div>
   );
 };
