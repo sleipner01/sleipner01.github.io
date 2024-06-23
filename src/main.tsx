9a29b88f2920df3ae35d793de91b176eabfc2d3b
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2024-06-23 15:55:40 +0200

    removed portfolio

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Portfolio from './pages/Portfolio.tsx';
 import TypeWriter from './pages/TypeWriter.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -12,8 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<TypeWriter />} />
-        <Route path='/portfolio' element={<Portfolio />} />
-        <Route path='*' element={<Portfolio />} />
+        <Route path='*' element={<TypeWriter />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
--- src/pages/TypeWriter.tsx
+++ src/pages/TypeWriter.tsx
@@ -1,7 +1,6 @@
 import './TypeWriter.scss';
 
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 
 import GithubIcon from '../assets/github-green.svg';
 import LinkedinIcon from '../assets/linkedin-green.svg';
@@ -34,16 +33,10 @@ const TypeWriter = () => {
         <p className='first'>Magnus Byrkjeland&apos;s Personal Website 🚀</p>
         <p className='second'>Sick right!?</p>
         <p className='third'>Maybe not what you thought you would find...</p>
-        <p className='fourth'>but I wanted the page to stand out!</p>
-        <p className='fifth'>You see, in good Norwegian I&apos;m &quot;miljøskadd&quot;...</p>
-        <p className='sixth'>My dad is an airline pilot.</p>
-        <p className='seventh'>So this page looks like the</p>
-        <p className='eighth'>communication displays found in the cockpit.</p>
-        <p className='ninth'>Though it kind of looks like an old computer screen too!</p>
         <br />
-        <p className='tenth'>Anyway...</p>
-        <p className='eleventh'>You can find my Github, LinkedIn &apos;n stuff below:</p>
-        <div className='twelvth socials-container'>
+        <p className='fourth'>Anyway...</p>
+        <p className='fifth'>You can find my Github, LinkedIn &apos;n stuff below:</p>
+        <div className='sixth socials-container'>
           <div className='github'>
             <a href='https://github.com/sleipner01' target='_blank' rel='noreferrer'>
               <img src={GithubIcon} alt='Github' />
@@ -55,13 +48,6 @@ const TypeWriter = () => {
             </a>
           </div>
         </div>
-        <p className='thirteenth'>Wow! You&apos;re still here?? Impressive!</p>
-        <p className='fourteenth'>K&apos; then, I have some more to show you...</p>
-        <p className='fifteenth'>
-          <Link to='/portfolio' className='link'>
-            Click me ❤️
-          </Link>
-        </p>
       </div>
     </>
   );
